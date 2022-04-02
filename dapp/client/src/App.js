03696afafeca8b10e3fe171734c5b085d5a67cbf
Dimitar <dvdimitrov13@gmail.com>
2022-04-02 11:26:33 +0300

    refactored code to functionnal components

--- dapp/client/src/App.js
+++ dapp/client/src/App.js
@@ -1,57 +1,61 @@
-import React, { Component } from "react";
+import React from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+export default function App() {
+  const [state, setState] = React.useState({ storageValue: 0, web3: null, accounts: null, contract: null })
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+  React.useEffect(() => {
+    async function web3Init() {
+      try {
+        // Get network provider and web3 instance.
+        const web3 = await getWeb3()
+  
+        // Use web3 to get the user's accounts.
+        const accounts = await web3.eth.getAccounts()
+  
+        // Get the contract instance.
+        const networkId = await web3.eth.net.getId()
+        const deployedNetwork = SimpleStorageContract.networks[networkId]
+        const instance = new web3.eth.Contract(
+          SimpleStorageContract.abi,
+          deployedNetwork && deployedNetwork.address,
+        );
+        
+        setState(value => ({ ...value, web3: web3, accounts: accounts, contract: instance }))
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+      } catch (error) {
+        // Catch any errors for any of the above operations.
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details.`,
+        );
+        console.error(error);
+      }
     }
-  };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+    web3Init()
+  }, [])
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+  React.useEffect(() => {
+    async function runExample (accounts, contract) {
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+      await contract.methods.set(5).send({ from: accounts[0] })
+      const response = await contract.methods.get().call()
+    
+      setState(value => ({ ...value, storageValue: response }))
+    };
+    if(state.accounts != null && state.contract != null) {
+      runExample(state.accounts, state.contract);
     }
+  }, [state.accounts, state.contract])
+
+  
+
+  if (!state.web3) {
+    return <div>Loading Web3, accounts, and contract...</div>;
+  } else {
     return (
       <div className="App">
         <h1>Good to Go!</h1>
@@ -64,10 +68,8 @@ class App extends Component {
         <p>
           Try changing the value stored on <strong>line 42</strong> of App.js.
         </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <div>The stored value is: {state.storageValue}</div>
       </div>
-    );
+    )
   }
 }
-
-export default App;
