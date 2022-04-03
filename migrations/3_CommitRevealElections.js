61f1aa17fd9ca4495fb533ea2c8293a1ebc25d01
Dimitar <dvdimitrov13@gmail.com>
2022-04-03 15:31:46 +0300

    introduced event listener

--- migrations/3_CommitRevealElections.js
+++ migrations/3_CommitRevealElections.js
@@ -3,9 +3,9 @@ var CommitRevealElections = artifacts.require("./CommitRevealElections.sol");
 module.exports = function(deployer) {
 
   deployer.deploy(CommitRevealElections, 
-                  60, /*_timeForProposal*/
-                  60, /*_timeForCommitment*/
-                  60, /*_timeForReveal*/
+                  0, /*_timeForProposal*/
+                  600, /*_timeForCommitment*/
+                  600, /*_timeForReveal*/
                   5, /*_maximumChoices*/
                   "Test Election", /*_ballotTitle*/ 
                   "0xCB5e34D036Cb39B2C00177a063CD3E8ecBa6Cd50", /*_owner*/
@@ -16,3 +16,5 @@ module.exports = function(deployer) {
                   1 /*_amountOfStake*/);
 
 };
+
+/*  0, 600, 600, 5, "Test Election", "0xCB5e34D036Cb39B2C00177a063CD3E8ecBa6Cd50", ["0x2E4a9C6CF87Cfe7e38907D784c744720D2C6ACdD","0xc7459e1100e7d9748a7900cC37bE5e2C93711CFf"], ["Biden","Obama","Trump"], 10, 2, 1 */
